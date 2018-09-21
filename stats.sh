f804aac6c27d6b10562bd7e007efae6b4b175373
Adam Forester <aforester@silverdogstudios.com>
2018-09-21 09:37:11 -0500

    Updated Base Build

--- stats.sh
+++ stats.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## This script will Check Memory, Disk, and CPU Utilization of a Gateway, and will also check Current and Peak FW Connections Table
+printf "$HOSTNAME current stats are:\n"
 free -g | awk 'NR==2{printf "Memory Usage: %s/%sGB (%.2f%%)\n", $3,$2,$3*100/$2 }'
 df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$4}'
 top -bn1 | grep load | awk '{printf "CPU Load: %.2f%%\n", $(NF-2)}'
